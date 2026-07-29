074bcd3cd088c6b5057108366cb013fdaed61840
MaroofSiddiqui <maroof8827@gmail.com>
2026-07-29 16:10:18 +0530

    Restructure backend into microservices

--- backend/src/main/java/com/sandbox/exception/AccountInactiveException.java
+++
@@ -1,8 +0,0 @@
-package com.sandbox.exception;
-
-public class AccountInactiveException extends RuntimeException {
-
-    public AccountInactiveException(String message) {
-        super(message);
-    }
-}
